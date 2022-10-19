61ae6c915fe61a0809d55293d724b03a270941a7
ShivamT-16 <114805714+ShivamT-16@users.noreply.github.com>
2022-10-19 23:40:19 +0530

    Add files via upload

--- logic2.cpp
+++ logic2.cpp
@@ -16,7 +16,7 @@ class node{
             this->right = NULL;
         }
 
-        node(char val,bool b){
+        node(char val,bool b){  // overloaded constructor to set truth value of atom
             this->atom = val;
             // making left and right pointers point to NULL
             this->left = NULL; 
@@ -36,11 +36,11 @@ class node{
             if (this == NULL){
                 return 0; }  
             else {
-            // finding the height of each subtree //
+            // finding the height of each subtree 
                 int height_left = this->left->height();
                 int height_right = this->right->height();
  
-            // We shall use the larger subtree to proceed further //
+            // We shall use the larger subtree to proceed further 
                 if (height_left > height_right){
                     return (height_left + 1);}
                 else{
@@ -50,28 +50,33 @@ class node{
     };
 
 bool eval(char op,bool b1){
+    // function to return complement of atom (NOT Implementation)
     if (op=='~'){
         return (!b1) ;}
     else{
-        return 0;
+        return -1;
     }
-}
+    }
+
 bool eval(char op,bool b1,bool b2){
+    // AND Implementation
     if (op=='*'){
         return (b1 && b2) ;
     }
+    // OR Implementation
     if (op=='+'){
         return (b1 || b2) ;
     }
+    // IMPLIES Implication
     if (op=='>'){
             return (eval('~',b1) || b2) ;
         }
     else{
-        return 0;
+        return -1;
     }
 }
 
-bool evaluate(node* n)
+bool evaluate(node* n)  // Function to evaluate parse tree and return result
 {
     if (n->left == NULL){
         if (n->right == NULL){
@@ -116,47 +121,38 @@ int main(){
     n7->left=NULL;
     n7->right=NULL;
 
-    bool result = evaluate(n0);
-    cout<<result<<endl;
+    cout<<"Welcome to our Project :)"<<endl<<endl;
+    cout<<"Please enter an Infix expression :";
+    cout<<endl<<endl<<"Creating Parse tree from given Prefix expression..."<<endl<<endl;
 
-    cout<<"In-order Traversal"<<endl;
-    n0->inOrder();
-    cout<<endl<<"Height of the tree "<<n0->height()<<endl;
-    n1 = new node('~');
-    n2 = new node('>');
-    n3 = new node('*');
-    n4 = new node('+');
-    n5 = new node('~');
-    n6 = new node('p',1);
-    n7 = new node('*');
-    node *n8 = new node('p',1);
-    node *n9 = new node('q',1);
-    node *n10 = new node('p',1);
-    node *n11 = new node('q',1);
-    n1->left = NULL;
-    n1->right = n2;
-    n2->left = n3;
-    n2->right = n4;
-    n4->left = n10;
-    n4->right = n11;
-    n3->left = n5;
-    n3->right = n7;
-    n7->left = n8;
-    n7->right = n9;
-    n8->left = NULL;
-    n8->right = NULL;
-    n9->left = NULL;
-    n9->right = NULL;
-    n5->left = NULL;
-    n5->right = n6;
-    n6->left = NULL;
-    n6->right = NULL;
-    n10->left = NULL;
-    n10->right = NULL;
-    n11->left = NULL;
-    n11->right = NULL;
-    result = evaluate(n1);
-    cout<<(result);
-
+    int n=1;
+    while(n<4){
+        cout<<"Please select an option"<<endl<<"1) Height of the tree"<<endl
+        <<"2) In-order Traversal of tree"<<endl
+        <<"3) Evaluating truth value of the expression"
+        <<endl;
+        int in1;
+        cin>>in1;
+        if(in1==1){
+            cout<<"Height of the tree = "<<n0->height()<<endl<<endl;
+            }
+        else if(in1==2)
+            {   
+                cout<<endl<<"In-order Traversal"<<endl;
+                n0->inOrder();
+                cout<<endl;
+                }
+        else if(in1==3){
+            bool result = evaluate(n0);
+            cout<<"Truth value of given expression is "<<result<<endl;
+            }
+        n++;
+        }
+        cout<<"Do you want to have restart(0 for no,1 for yes): ";
+        int in2;
+        cin>>in2;cout<<endl;
+        if (in2==1){main();}
+        else{cout<<"Thank You!!";
+            return 0;}
     return 0;
 }
