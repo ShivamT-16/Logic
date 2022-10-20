4bf77961478524517c82c9a566b13c25ee1439b4
Tanish32 <110172756+Tanish32@users.noreply.github.com>
2022-10-20 20:28:15 +0530

    Add files via upload

---
+++ logicQ2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+	char data;
+	node *left;
+	node *right;
+
+	node(char c)
+	{
+		data = c;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+char *treeCreator(node **node_holder, char *atom)
+{
+	if (*atom == '\0')
+		return NULL;
+	//
+	while (1)
+	{
+		char *temp = NULL;
+		if (*node_holder == NULL)
+		{
+			node *nn = new node(*atom);
+			*node_holder = nn;
+		}
+		else
+		{
+			if (*atom >= 'a' && *atom <= 'z')
+			{
+				return atom;
+			}
+			temp = treeCreator(&((*node_holder)->left), atom + 1);
+			temp = treeCreator(&((*node_holder)->right), temp + 1);
+			return temp;
+		}
+	}
+}
+
+int main()
+{
+	node *root = NULL;
+	string prefix = ">p*qr";
+	treeCreator(&root, &prefix[0]);
+	return 0;
+}
